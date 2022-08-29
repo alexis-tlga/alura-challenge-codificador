8a202644b183af9e5b82113fdf380d9c075fb0c0
alexis-tlga <maruchantl98@gmail.com>
2022-08-29 18:46:11 -0400

    encode and decode working

---
+++ js/utils.js
@@ -0,0 +1,63 @@
+const input = document.getElementById('text-input');
+const output = document.getElementById('text-output');
+const initialOutput = document.getElementById('initial-output');
+const resultOutput = document.getElementById('result-output');
+input.onpaste = forceLowerCasePaste;
+
+
+const characterMap = {
+	'e': 'enter',
+	'i': 'imes',
+	'a': 'ai',
+	'o': 'ober',
+	'u': 'ufat',
+}
+
+
+function encode(text) {
+	for (let key in characterMap) {
+		text = text.replaceAll(key, characterMap[key]);
+	}
+	return text;
+}
+
+function decode(text) {
+	reverseKeys = Object.keys(characterMap).reverse();
+	for (let key of reverseKeys) {
+		text = text.replaceAll(characterMap[key], key);
+	}
+	return text;
+}
+
+function encodeButtonEvent() {
+	const text = input.value;
+	const encodedText = encode(text);
+	if (initialOutput.style.display !== 'none') {
+		initialOutput.style.display = 'none';
+	}
+	if (resultOutput.style.display === 'none') {
+		resultOutput.style.display = 'block';
+	}
+	output.innerHTML = encodedText;
+}
+
+function decodeButtonEvent() {
+	const text = input.value;
+	const decodedText = decode(text);
+	if (initialOutput.style.display !== 'none') {
+		initialOutput.style.display = 'none';
+	}
+	if (resultOutput.style.display === 'none') {
+		resultOutput.style.display = 'block';
+	}
+	output.innerHTML = decodedText;
+}
+
+function forceLowerCase(event) {
+	input.value = input.value.toLowerCase();
+}
+
+function forceLowerCasePaste(event) {
+	copiedText = event.clipboardData.getData('text/plain');
+	input.value = copiedText.toLowerCase();
+}
